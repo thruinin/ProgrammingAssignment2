8430c10bc468c6a433953917016d5235e7b6cc34
Thruinin <tomas.maccor@gmail.com>
2017-08-05 23:26:14 -0300

    Apologies I saved all intermediate files into my HDD hence this is the only version of file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Returns a list of functions that can perform operations to an R object
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) 
+  {
+  inv = NULL
+  
+  set = function(y) {
+    # use `<<-` to assign a value to an object in an environment 
+    # different from the current environment. 
+    x <<- y
+    inv <<- NULL   }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  
+  }
 
-}
 
 
-## Write a short comment describing this function
+## Computes the inverse of matrix given @MakecacheMatrix, 
+## but first it checks if result is already stored in variable inv, if so it returns cached value
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x, ...)
+  {
+  inv = x$getinv()
+  
+  # if the inverse has already been calculated
+  if (!is.null(inv)){
+    message("getting cached data")
+    return(inv)  }
+  
+  # otherwise, calculates the inverse 
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  # sets the value of the inverse in the cache via the setinv function.
+  x$setinv(inv)
+  inv
+  
+  }
